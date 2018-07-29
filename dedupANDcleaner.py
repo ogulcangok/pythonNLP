7d06faa6f7aa2129623a9cc90f3783a047ce6e3d
asuerdem1 <41635882+asuerdem1@users.noreply.github.com>
2018-07-29 11:38:45 +0300

    Update dedupANDcleaner.py

--- dedupANDcleaner.py
+++ dedupANDcleaner.py
@@ -38,7 +38,7 @@ any data science problem you have and in particular now for text classification.
 """ 
 1. check if there's missing data"
 ""
-df.isnull().sum() #sil diye bir şey ekenilir, na omit
+df.isnull().sum() #bu na remove gibi bir komutla replace edielecek
 
 """
 2. Duplicates
@@ -113,30 +113,15 @@ dat = dat.drop(dat.index[jac['Doc2']])
 Drop the corresponing indexes from the main dataframe dat
 """
 
-""" 
-3 Inspect categorical  variables, aburada source amam yrıca diğer categoric variables için de yapılabilir
-check what sources we have, burada eğer çok az sayıda olan source varsa çıkarma fonksiyonu eklenebilir"
-""
-nmb = len(df.source.value_counts().index)
 
-"""barchart of sources"""
-fig, ax = plt.subplots(1,1,figsize=(8,6))
-source_vc = df.source.value_counts()
-
-ax.bar(range(nmb), source_vc)
-ax.set_xticks(range(nmb))
-ax.set_xticklabels(source_vc.index, fontsize=11)
-for rect, c, value in zip(ax.patches, ['b', 'r'], source_vc.values): # , 'g', 'y', 'c', 'm'
-    rect.set_color(c)
-    height = rect.get_height()
-    width = rect.get_width()
-    x_loc = rect.get_x()
-    ax.text(x_loc + width/2, 0.5*height, value, ha='center', va='center', fontsize=10, color='white')
     
 """
-This is for cleaning the source, add other categories if they are in the metadata, also find something for the date format as well
+3. This is for cleaning the source, add other categories if they are in the metadata, also find something for the date format as well
 there are two steps to this: 
 """
+"""3.1. clean the metadata, categorical variables such as sıource, section etc. 
+burada, bir önceki explore your data da bulduklarını girecek, yani çıkarılmasını istediklerini yazacak arayüzde"""
+
 def extract_source(source):
     """
     splits the './' etc. from the source column
@@ -153,65 +138,35 @@ def extract_source(source):
 
 dat['source'] = dat['source'].apply(lambda x: extract_source(x)) 
 
+""" 3.2 convert date , and extract year and month as separate colmuns, buraya gerkli kod gir"""
 
-"""4 Inspect text variables (i.e title, content), min max number"""
-
-"""4.1  özet"""
+"""3.3  clean shorter - longer docs,"""
 document_lengths = np.array(list(map(len, df.content.str.split(' '))))
-print("The average number of words in a document is: {}.".format(np.mean(document_lengths)))
-print("The minimum number of words in a document is: {}.".format(min(document_lengths)))
-print("The maximum number of words in a document is: {}.".format(max(document_lengths)))    
+df[document_lengths <= 50]
+""" filter out  shorter docs)"""
+df = df[document_lengths > 50] # 50 will change according to introduced vaue
+""" filter out  longr docs)"""
+df = df[document_lengths > 2000] # 2000 will change according to introduced vaue
 
 
 """"following steps needs to be done
+        also, sentence tokenizer, and sentence counter neeeded
     removes empty lines
     removes redundant space characters
     drops lines (and their corresponding lines), that are empty, too short, too long
-    also, sentence tokenizer, and sentence counter neeeded
     """
     
-    
-    
-""" 4.2  word distribution by doc"""
-fig, ax = plt.subplots(figsize=(15,6))
-ax.set_title("Distribution of number of words", fontsize=16)
-ax.set_xlabel("Number of words")
-sns.distplot(document_lengths, bins=50, ax=ax);
-"""detecting very short docs"""
-print("There are {} documents with over 100 words.".format(sum(document_lengths > 100)))
 
-"""4..3 distribution in shorter docs, this is to decide if any docs should be excluded"""
-shorter_documents = document_lengths[document_lengths <= 100]
-fig, ax = plt.subplots(figsize=(15,6))
-ax.set_title("Distribution of number of words", fontsize=16)
-ax.set_xlabel("Number of words")
-sns.distplot(shorter_documents, bins=50, ax=ax);
+"""
+    4. preporcess the text
+""
 
-"""4.4 list of shorter docs, 50 in this case, but change according to the analyses above"""
-df[document_lengths <= 50]
-""" filter out  shorter docs)"""
-df = df[document_lengths > 50]
-    
+"""4.1 spell corrector", this takes long time to finish, find if there is a faster way, or maybe ommited, or a warning message"""
 
-
-"""FEATURE EXTRACTION, 
-prporcess the text
-
-The usual approach in natural language processing, is to first kind of cleanse the text. We have to make sure our model can understand similarities and understand when two different words mean similar things. You can't just input the raw text into a model and expect it to understand everything. Therefor, I summarized the most common steps to take in order to achieve this cleansing.
-    Remove words that mean little; these are usually words that occur very frequently or words that occur very infrequently. 
-    Also, punctuation can be removed, but perhaps you'd like to keep some punctuation in, 
-    like exclamation marks or question marks since maybe one writer uses them more than others.
-    Stemming; this basically means joining words that mean the same. Take for example the words running and runs, 
-    the stem of both words is run. Thus with stemming you'd group these words together and give them the same meaning for the model.
-    Vectorize words; since we can't input plain words into a model and expect it to learn from it, we have to vectorize the words. 
-    This basically means create unit vectors for all words.
-."""
-
-"""1 spell corrector", this takes long time to finish, find if there is a faster way"""
 from textblob import TextBlob
 df['content'][:5].apply(lambda x: str(TextBlob(x).correct()))
 
-""" 2 remove non-ascii words"""
+""" 4. 2 remove non-ascii words"""
 df = df.reset_index(drop=True)
 our_special_word = 'nonascii'
 def remove_ascii_words(df):
@@ -227,7 +182,7 @@ print("Replaced {} words with characters with an ordinal >= 128 in the train dat
     len(non_ascii_words)))
 
 
-""" 3  text CLEANER, ADD more regexes, more soecific to news"""
+""" 4.3 text CLEANER, ADD more regexes, more soecific to news"""
 """Convert content to list, and clean the text with regex"""
 data = df.content.values.tolist()
 # Remove Emails
@@ -238,33 +193,6 @@ data = [re.sub('\n+', ' ', content) for content in data]
 data = [re.sub('\r+', ' ', content) for content in data]
 # Remove distracting single quotes
 data = [re.sub("\'", "", content) for content in data]
-
-
-
-""" 4 Inspect this WC for any extreöme non sense words,  
- to detect extreme words, irregular characters etc... if nonsense, then include them in the SW list"""
-
-"""this is for flattening the list and bringing all strings into one text"""
-all_text = ' '.join([text for text in df['content']])
-print('Number of words in all_text:', len(all_text))
-
-"""wordcloud"""
-from wordcloud import WordCloud, STOPWORDS
-wordcloud = WordCloud(width = 800, height = 800,
-                background_color ='white',
-                stopwords = STOPWORDS,
-                min_font_size = 20).generate(all_text)
-plt.figure(figsize = (8, 8), facecolor = None)
-plt.imshow(wordcloud)
-plt.axis("off")
-plt.tight_layout(pad = 0)
-plt.show()
-   
-    
-"""MORE TO ADD, lexical diversity, readability, lexical richness etc..."""
-"""
-
-
 
 """
 Apply the extract_source function to dataframes source column and overwrite it
